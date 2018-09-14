6d9ffeadd57114bcde0bfb14ddab0638270216f3
pehtoorismies <pehtoorismies@gmail.com>
2018-09-14 15:05:33 +0300

    add cors

--- src/lambdas/verifyemail.js
+++ src/lambdas/verifyemail.js
@@ -2,11 +2,30 @@ require('dotenv').config();
 
 const emails = process.env.EMAIL_WHITE_LIST;
 
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+};
+
 /* eslint-disable */
 export function handler(event, context, callback) {
-  console.log(event);
+  const { path } = event;
+
+  console.log(path);
+  const pathArray = path.split('/');
+  if (pathArray.length != 3) {
+    return callback('Not found');
+  }
+  if (pathArray[1] !== 'verifyemail') {
+    return callback('Not found');
+  }
+  const email = pathArray[2];
+
+
+
   callback(null, {
+    headers,
     statusCode: 200,
-    body: JSON.stringify({ msg: emails }),
+    body: JSON.stringify({ msg: email }),
   });
 }
